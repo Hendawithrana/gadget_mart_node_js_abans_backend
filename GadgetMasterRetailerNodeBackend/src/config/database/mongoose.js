d37826db53a50564870fd6a2e634f52078b25eee
Ramesh97 <hwr@1997>
2022-03-06 07:15:32 +0530

    vesrion 01

--- GadgetMasterRetailerNodeBackend/src/config/database/mongoose.js
+++ GadgetMasterRetailerNodeBackend/src/config/database/mongoose.js
@@ -2,7 +2,6 @@ const logger = require('../logger/logger')
 const mongoose = require('mongoose')
 mongoose.Promise = require('bluebird')
 
-// const dbURL ="mongodb+srv://uvindu:Uvin1234@webservice-abans.mg6kd.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const dbURL ="mongodb+srv://dilshan:Apple500@ramesh-pc.vnwzj.mongodb.net/gadgetMart?retryWrites=true&w=majority";
 
 // refere this link for more details - https://mongoosejs.com/docs/connections.html
